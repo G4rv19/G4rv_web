3a65920d01bf734f4c75f09072de1a27255b87f2
G4rv19 <garvsidana29@gmail.com>
2024-04-27 15:18:10 +1000

    sss

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('web.html')  # Remove '/templates/' from the template path
+    return render_template('web.html')
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8000)
 
